d34140c15d47eedb421a86b298de10a3535f9817
Calum Sieppert <sieppertcalum@gmail.com>
2022-07-21 08:06:57 -0600

    Remove unnecessary cnft testing function

--- src/Seabug/CnftMintPolicy.purs
+++ src/Seabug/CnftMintPolicy.purs
@@ -1,25 +1,12 @@
-module Seabug.CnftMintPolicy
-  ( mkCnftMintingPolicy
-  , main
-  ) where
+module Seabug.CnftMintPolicy (mkCnftMintingPolicy) where
 
 import Contract.Prelude
 
-import Contract.Monad
-  ( Contract
-  , defaultTestnetContractConfig
-  , launchAff_
-  , liftContractE
-  , liftedE
-  , runContract_
-  )
+import Contract.Monad (Contract, liftContractE)
 import Contract.PlutusData (toData)
-import Contract.Prim.ByteArray (hexToByteArrayUnsafe)
 import Contract.Scripts (MintingPolicy, applyArgs)
 import Contract.Transaction (TransactionInput)
-import Contract.Value (scriptCurrencySymbol)
 import Data.Argonaut (Json, JsonDecodeError)
-import Data.UInt (fromInt)
 import QueryM as QueryM
 import Seabug.Helpers (jsonReader)
 
@@ -30,20 +17,6 @@ mkCnftMintingPolicy
 mkCnftMintingPolicy oref = do
   p <- liftContractE cnftMintingPolicy
   applyArgs p [ toData oref ]
-
-main :: Effect Unit
-main = launchAff_ $ do
-  cfg <- defaultTestnetContractConfig
-  runContract_ cfg $ do
-    policy <- liftedE $ mkCnftMintingPolicy
-      ( wrap
-          { transactionId: wrap $ hexToByteArrayUnsafe
-              "00e52d6fdb45e529dda8cfaa4e7f04dc8b94deffd1bd54196193cc2c5c49e418"
-          , index: fromInt 5
-          }
-      )
-    log $ show policy
-    log <<< show =<< (liftAff $ scriptCurrencySymbol policy)
 
 cnftMintingPolicy :: Either JsonDecodeError MintingPolicy
 cnftMintingPolicy = jsonReader "mintingPolicy" _cnftMintingPolicy
